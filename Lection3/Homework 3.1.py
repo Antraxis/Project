a268050fc3a4ccb185032b747e417236cf13f908
Павел Крамаренко <antraxis111@gmail.com>
2025-03-23 17:48:36 +0100

    homework3.1

---
+++ Lection3/Homework 3.1.py
@@ -0,0 +1,12 @@
+num_1=int(input('number1'))
+effect =input('effect')
+num_2=int(input('number2'))
+if effect == '+':
+   print(num_1 + num_2)
+elif effect == '-':
+    print(num_1-num_2)
+elif effect=='*':
+    print(num_1*num_2)
+elif effect=='/':
+    print(num_1/num_2)
+
