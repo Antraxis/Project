80ddcc596fb7c95d878161325c32959185ecfcef
Павел Крамаренко <antraxis111@gmail.com>
2025-03-23 17:17:30 +0100

    homework3.2

--- Lection3/homework3.2.py
+++ Lection3/homework3.2.py
@@ -1,7 +1,10 @@
-one=[1,2,3,4,5,6]
+#one=[1,2,3,4,5,6]
 #one=[1,2,3,4]
-#one=[1]
+one=[1]
 #one=[]
-two=one.pop()
-one.insert(0,two)
-print(one)
+if len(one)==0:
+    print([])
+else:
+    two=one.pop()
+    one.insert(0,two)
+    print(one)
