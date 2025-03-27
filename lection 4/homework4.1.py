7a1807a3793dc5b8d7a11b7230a34539985f3d06
Павел Крамаренко <antraxis111@gmail.com>
2025-03-27 18:15:57 +0100

    homework4.1

--- lection 4/homework4.1.py
+++ lection 4/homework4.1.py
@@ -4,8 +4,8 @@ one=[9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 #one=[0]
 for two in one:
     if two==0:
-        u=one.index(two)
-        y=one.pop(u)
-        one.append(y)
+        index=one.index(two)
+        take=one.pop(index)
+        one.append(take)
 print(one)
 
