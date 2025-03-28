7badfcdc1b640e95e9d2c9b5a09016a6cebaecec
Павел Крамаренко <antraxis111@gmail.com>
2025-03-28 21:49:21 +0100

    homework4.3

--- lection 4/homework4.3.py
+++ lection 4/homework4.3.py
@@ -1,20 +1,23 @@
-#one=[1, 2, 3, 4, 5, 6, 7, 9]
-one=[1, 1, 2, 1]
-#one=[6, 3, 7]
-good=[]
+import random
+
+one=[]
+for lst in range(random.randint(3,10)):
+    one.append(random.randint(0,50))
+print(one)
+result=[]
 for two,u in enumerate(one):
     if two==1:
         y=one.copy()
         x=y.pop(0)
-        good.append(x)
+        result.append(x)
         continue
     elif two==2:
         y=one.copy()
         x=y.pop(2)
-        good.append(x)
+        result.append(x)
 r=one.pop(-2)
-good.append(r)
-print(good)
+result.append(r)
+print(result)
 
 
 
