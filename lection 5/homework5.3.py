3da35bf522d187275ffd8290d9a1bdbe49271d05
Павел Крамаренко <antraxis111@gmail.com>
2025-03-29 19:51:36 +0100

    homework5.3

---
+++ lection 5/homework5.3.py
@@ -0,0 +1,11 @@
+one = input("enter")
+one_x= one.replace(".","").replace(",","").replace("!","").replace("?","")
+separator=one_x.split()
+try_2=""
+for try_0 in separator:
+      try_1=try_0.capitalize()
+      try_2+=try_1
+try_3="#"
+summa=try_3+try_2
+result=summa[:140]
+print(result)
